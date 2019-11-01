291c778fb8918d8825acfe784de6743fb5c0bd67
Yuriy <JMFedorov@gmail.com>
2019-11-01 16:28:37 +0300

    iterator is done

---
+++ iterator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+class MyIterator:
+    def __init__(self, iter_obj, only_even=True):
+        self._iter_obj = iter(iter_obj)
+        self._even = only_even
+        if only_even:
+            self._is_to_return = False
+        else:
+            self._is_to_return = True
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            try:
+                res = next(self._iter_obj)
+                if self._is_to_return:
+                    self._is_to_return = not self._is_to_return
+                    return res
+                self._is_to_return = not self._is_to_return
+            except StopIteration:
+                print('End of custom iterator')
+                raise StopIteration
+
+
+m = MyIterator([1, 2, 3, 4, 5, 6, 7, 8], False)
+for e in m:
+    print(e)
