c3f0fbc0f759c69f6c026545230f7bdfccec7ec9
Yuriy <JMFedorov@gmail.com>
2019-11-04 10:53:17 +0300

    threading done

--- dothreethings.py
+++ dothreethings.py
@@ -1,12 +1,51 @@
+import threading
+import time
+
+
 class Doer:
+    def __init__(self):
+        self.whos_turn = 1
+
     def first(self, action):
+        while self.whos_turn != 1:
+            time.sleep(1)
+        print('First')
         # Следующую строчку не убирать
         action()
+        self.whos_turn = 2
     
     def second(self, action):
+        while self.whos_turn != 2:
+            time.sleep(1)
+        print('Second')
         # Следующую строчку не убирать
         action()
+        self.whos_turn = 3
     
     def third(self, action):
+        while self.whos_turn != 3:
+            time.sleep(1)
+        print('Third')
         # Следующую строчку не убирать
         action()
+        self.whos_turn = 1
+
+
+def action():
+    print('Processing')
+    time.sleep(2)
+    print('Done')
+
+
+pool = []
+doer = Doer()
+object_methods = [method_name for method_name in dir(doer)
+                  if '_' not in method_name and callable(getattr(doer, method_name))]
+for m in object_methods:
+    thread = threading.Thread(target=getattr(doer, m), args=(action,))
+    thread.start()
+    pool.append(thread)
+    time.sleep(0.5)
+for t in pool:
+    t.join()
+
