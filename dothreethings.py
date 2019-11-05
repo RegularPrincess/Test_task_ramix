e54029d2455a7ab2a2ea82fee9646dd88c25756e
Yuriy <JMFedorov@gmail.com>
2019-11-05 13:13:58 +0300

    sinhronize via Condition object

--- dothreethings.py
+++ dothreethings.py
@@ -1,30 +1,39 @@
-import threading
+from threading import Condition, Thread
 import time
 
 
 class Doer:
     def __init__(self):
         self.whos_turn = 1
+        self.cond_1_2 = Condition()
+        self.cond_2_3 = Condition()
+
+    def _wait_for_turn(self, turn, condition):
+        with condition:
+            while self.whos_turn != turn:
+                condition.wait()
+            return
+
+    def _let_go_next(self, turn, condition):
+        self.whos_turn = turn
+        with condition:
+            condition.notify()
 
     def first(self, action):
-        while self.whos_turn != 1:
-            time.sleep(1)
         print('First')
         # Следующую строчку не убирать
         action()
-        self.whos_turn = 2
-    
+        self._let_go_next(turn=2, condition=self.cond_1_2)
+
     def second(self, action):
-        while self.whos_turn != 2:
-            time.sleep(1)
+        self._wait_for_turn(turn=2, condition=self.cond_1_2)
         print('Second')
         # Следующую строчку не убирать
         action()
-        self.whos_turn = 3
+        self._let_go_next(turn=3, condition=self.cond_2_3)
     
     def third(self, action):
-        while self.whos_turn != 3:
-            time.sleep(1)
+        self._wait_for_turn(turn=3, condition=self.cond_2_3)
         print('Third')
         # Следующую строчку не убирать
         action()
@@ -42,8 +51,8 @@ if __name__ == '__main__':
     doer = Doer()
     object_methods = [method_name for method_name in dir(doer)
                       if '_' not in method_name and callable(getattr(doer, method_name))]
-    for m in object_methods:
-        thread = threading.Thread(target=getattr(doer, m), args=(action,))
+    for m in object_methods[::-1]:
+        thread = Thread(target=getattr(doer, m), args=(action,))
         thread.start()
         pool.append(thread)
         time.sleep(0.5)
