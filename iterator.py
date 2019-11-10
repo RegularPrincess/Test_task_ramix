2f79c32d2c1c9b4e7ee4d2613a59edd1f2b077ac
Yuriy <JMFedorov@gmail.com>
2019-11-10 23:23:39 +0300

    Simplify iterator

--- iterator.py
+++ iterator.py
@@ -5,28 +5,26 @@ class MyIterator:
     def __init__(self, iter_obj, only_even=True):
         self._iter_obj = iter(iter_obj)
         self._even = only_even
-        if only_even:
-            self._is_to_return = False
-        else:
-            self._is_to_return = True
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        while True:
-            try:
+        try:
+            if self._even:
+                next(self._iter_obj)
                 res = next(self._iter_obj)
-                if self._is_to_return:
-                    self._is_to_return = not self._is_to_return
-                    return res
-                self._is_to_return = not self._is_to_return
-            except StopIteration:
-                print('End of custom iterator')
-                raise StopIteration
+                return res
+            else:
+                res = next(self._iter_obj)
+                next(self._iter_obj)
+                return res
+        except StopIteration:
+            print('End of custom iterator')
+            raise StopIteration
 
 
 if __name__ == '__main__':
-    m = MyIterator([1, 2, 3, 4, 5, 6, 7, 8], False)
+    m = MyIterator([1, 2, 3, 4, 5, 6, 7, 8], True)
     for e in m:
         print(e)
