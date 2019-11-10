d4392d545e5bc486004ab87d6e376e2c07e4eeba
Yuriy <JMFedorov@gmail.com>
2019-11-10 22:07:12 +0300

    decorator exeption handle

--- decorator.py
+++ decorator.py
@@ -7,11 +7,16 @@ import time
 def measure_time(method):
     def timed(*args, **kw):
         ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        print('%r затрачено %2.2f секунд' % \
-              (method.__name__, te-ts))
-        return result
+        result = None
+        try:
+            result = method(*args, **kw)
+        except Exception:
+            pass
+        finally:
+            te = time.time()
+            print('%r затрачено %2.2f секунд' % \
+                  (method.__name__, te-ts))
+            return result
 
     return timed
 
@@ -20,6 +25,7 @@ def measure_time(method):
 def fun1():
     print('fun3 start')
     time.sleep(1)
+    int('f')
     return 'fun3 finish'
 
 
